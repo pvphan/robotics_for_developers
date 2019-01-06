be0ca5b594f200fceb37f343cbffcb2577ddc26e
Vinh Phan <vinh@traptic.com>
2019-01-05 16:45:55 -0800

    reading and writing rosbag working

--- r4d_1_single_marker/scripts/process_rosbag.py
+++ r4d_1_single_marker/scripts/process_rosbag.py
@@ -1,5 +1,26 @@
 import cv2
+import rosbag
 from rcars_detector_msgs.msg import Tag
 
-inputPath = "/media/nvidia/nvme256/rcarsdata/dataset_table.bag"
-outputPath = "/media/nvidia/nvme256/fiducial_slam/dataset_table_tagged.bag"
+def main():
+    inputPath = "/media/nvidia/nvme256/rcarsdata/dataset_table.bag"
+    outputPath = "/media/nvidia/nvme256/fiducial_slam/dataset_table_tagged.bag"
+    allTopics = [ "/cam0/camera_info", "/cam0/image_raw", "/imu0", ]
+
+    try:
+        inputBag = rosbag.Bag(inputPath)
+        outputBag = rosbag.Bag(outputPath, "w")
+
+        for topic, msg, t in inputBag.read_messages(topics=allTopics):
+            print(topic, t)
+            if topic == "/tf" and msg.transforms:
+                outputBag.write(topic, msg, msg.transforms[0].header.stamp)
+            else:
+                outputBag.write(topic, msg, msg.header.stamp if msg._has_header else t)
+
+    finally:
+        inputBag.close()
+        outputBag.close()
+
+if __name__ == "__main__":
+    main()
